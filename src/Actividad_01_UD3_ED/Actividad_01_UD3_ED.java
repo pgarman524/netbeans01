50d612589849f5d7531fbace419d7face984bdf1
pgarman524 <pgarman524@g.educaand.es>
2023-12-04 09:52:26 +0100

    Netbeans primer ejercicio

---
+++ src/Actividad_01_UD3_ED/Actividad_01_UD3_ED.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Actividad_01_UD3_ED;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+/**
+ *
+ * @author ana
+ */
+/**
+ * 
+ * @author pablo
+ * 
+ * 2º Versión traducción español
+ * 
+ * Modifica las variables:
+ * buffer = intermedio
+ * fileName = nombreArchivo
+ * inputStream = flujoEntrada
+ * inputBuffer = bufferEntrada
+ * 
+ */
+public class Actividad_01_UD3_ED 
+{
+    private static byte[] intermedio = new byte[1000];
+    private static String nombreArchivo = "fichero.dat";
+    private static FileInputStream flujoEntrada = null;
+    private static BufferedInputStream bufferEntrada = null;
+
+    public static void inicializateFiles() throws FileNotFoundException
+    {
+        flujoEntrada = new FileInputStream(nombreArchivo);
+        bufferEntrada = new BufferedInputStream(flujoEntrada);
+    }
+    
+    public static int showFileText() throws IOException
+    {
+        int total = 0;
+        int nRead = 0;
+        while((nRead = flujoEntrada.read(intermedio)) != -1) 
+        {
+            System.out.println(new String(intermedio));
+            total += nRead;
+        }
+        
+        return total;
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {        
+        try 
+        {
+            inicializateFiles();
+            
+            int total = showFileText();           
+
+            System.out.println("\nLeídos " + total + " bytes");
+        }
+        catch(IOException ex) 
+        {
+            System.out.println("Error -> " + ex.toString());                
+        }
+        finally 
+        {
+            try 
+            {
+                if( bufferEntrada != null && flujoEntrada != null )
+                {
+                    flujoEntrada.close();
+                    bufferEntrada.close();
+                }                
+            } 
+            catch (IOException ex) 
+            {
+                System.out.println("Error al cerrar el fichero -> " + ex.toString());
+            }
+        }
+    }
+    
+}
